7122842ad0675598593b6b2fb8f525b6b1afd8b4
Neon Riot <sbrookshire99@gmail.com>
2021-05-27 22:44:53 -0500

    product routes are finished

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -18,9 +18,18 @@ router.get("/", async (req, res) => {
 });
 
 // get one product
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [{ all: true }],
+      where: { id: req.params.id },
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -99,6 +108,14 @@ router.put("/:id", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   // delete one product by its `id` value
+  try {
+    rowsAffected = await Product.destroy({
+      where: { id: req.params.id },
+    });
+    res.status(200).json(rowsAffected);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
