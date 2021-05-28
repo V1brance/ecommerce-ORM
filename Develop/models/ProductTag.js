26048e42e3eee632fd0d113ec15289c55630fca6
Neon Riot <sbrookshire99@gmail.com>
2021-05-27 21:26:42 -0500

    Forgot to initial commit, finished all column models and seeded database

---
+++ Develop/models/ProductTag.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require("sequelize");
+
+const sequelize = require("../config/connection");
+const Product = require("./Product");
+const Tag = require("./Tag");
+
+class ProductTag extends Model {}
+
+ProductTag.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Product,
+        key: id,
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: Tag.id,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "product_tag",
+  }
+);
+
+module.exports = ProductTag;
